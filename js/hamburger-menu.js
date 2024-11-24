c2a7772b3593ac2c8081016dc3f776c4093069ae
Max-207 <153686338+Max-207@users.noreply.github.com>
2024-11-24 13:36:37 +0100

    Update hamburger-menu.js

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -4,11 +4,18 @@ const headerDropdownElement = document.querySelector(".header-dropdown");
 hamburgerMenuElement.addEventListener("click", () => {
     hamburgerMenuElement.classList.toggle("opened");
     if(hamburgerMenuElement.classList.contains("opened")) {
-        headerDropdownElement.style.translate = "0";
-        hamburgerMenuElement.style.translate = parseInt(window.getComputedStyle(headerDropdownElement).width) * -1 + "px";
+        headerDropdownElement.style.display = "inline";
+        setTimeout(() => {
+            headerDropdownElement.style.translate = "0";
+            hamburgerMenuElement.style.translate = parseInt(window.getComputedStyle(headerDropdownElement).width) * -1 + "px";
+        }, 0);
+
     }
     else {
-        headerDropdownElement.style.translate = "100%";
-        hamburgerMenuElement.style.translate = 0;
+        headerDropdownElement.style.display = "none";
+        setTimeout(() => {
+            headerDropdownElement.style.translate = "100%";
+            hamburgerMenuElement.style.translate = 0;
+        }, 0);
     }
 });
